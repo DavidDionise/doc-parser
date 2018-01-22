83fb43dd16d2c0a2f235dfc492349077bc9d931d
David Dionise <david@hailtrace.com>
2018-01-21 22:17:44 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div className='app-continer'>
+      HELLO WOOOOORLDssss
+    </div>
+  )
+}
+
+export default App;
